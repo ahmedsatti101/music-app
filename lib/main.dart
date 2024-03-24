1a4956c611816946ea69276c51d289f38f734c2b
Ahmed Mohamed <ahmedysatti@gmail.com>
2024-03-24 15:41:57 +0000

    Added a navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:demo_music_app/presentation/widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:demo_music_app/presentation/pages/home.dart' as HomePage;
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage.HomePage(),
+      home: const NavigationBarWidget(),
     );
   }
 }
